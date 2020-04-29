2ef2f76cb8b0fd9d2bd8882442f2b514fe7692d8
flurrux <40806446+flurrux@users.noreply.github.com>
2020-04-30 00:32:43 +0200

    Delete types.ts

--- types.ts
+++
@@ -1,3 +0,0 @@
-export type Matrix3 = [number, number, number, number, number, number, number, number, number];
-export type Vector3 = [number, number, number];
-export type Vector2 = [number, number];
